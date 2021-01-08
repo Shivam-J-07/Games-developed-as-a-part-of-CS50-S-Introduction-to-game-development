6a8ecfe7a9829c1488fa5595d4a3d11874fb2675
Shivam-J-07 <shivamganeshjindal07@gmail.com>
2021-01-07 20:40:16 -0500

    2.5D helicopter game

---
+++ 2.5D helicopter game/Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawner : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	// Use this for initialization
+	void Start () {
+
+		// infinite coin spawning function, asynchronous
+		StartCoroutine(SpawnGems());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator SpawnGems() {
+		while (true) {
+
+			Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+
+			// pause 7-10 seconds until the next coin spawns
+			yield return new WaitForSeconds(Random.Range(7, 10));
+		}
+	}
+}
